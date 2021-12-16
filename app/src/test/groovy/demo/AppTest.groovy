d33ec95ec02140539df631eb711e547ae4a911f4
fred eisele <fred.eisele@vanderbilt.edu>
2021-12-16 13:44:19 -0600

    Initial commit

---
+++ app/src/test/groovy/demo/AppTest.groovy
@@ -0,0 +1,19 @@
+/*
+ *
+ */
+package demo
+
+import spock.lang.Specification
+
+class AppTest extends Specification {
+    def "application has a greeting"() {
+        setup:
+        def app = new App()
+
+        when:
+        def result = app.greeting
+
+        then:
+        result != null
+    }
+}
